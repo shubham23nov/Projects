6a6fc8d3e25644c1f264939cab14881cd4704585
shubham23nov <47450625+shubham23nov@users.noreply.github.com>
2019-03-17 22:14:18 +0530

    Web Service Health Check

---
+++ Web-Service-Health-Check-master/WebServiceHealthCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WebServiceHealthCheck
+{
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    #endregion
+
+    /// <summary>
+    /// The class with main entry point for the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ServiceCenter());
+        }
+    }
+}
